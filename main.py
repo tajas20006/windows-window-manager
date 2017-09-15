c959e7ae5cc872d95aeea5d6e6f7045a0ca707c2
Yoshioka Shun <tajas2006@yahoo.co.jp>
2017-09-15 12:04:23 +0900

    use threading to keep the windows where they should be

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import keyhandler
 import window_manager
 import sys
+import threading
+import time
 
 class EntryPoint():
     def __init__(self, manager):
@@ -12,35 +14,43 @@ class EntryPoint():
         self.manager = manager
 
     def print_event(self, e, mod_flag):
-        if mod_flag & self.noconvert:
-            if mod_flag & self.shift:
-                if e.key_code == ord('A'):
-                    manager.send_active_window_to_nth_stack(0)
-                elif e.key_code == ord('S'):
-                    manager.send_active_window_to_nth_stack(1)
-                elif e.key_code == ord('C'):
-                    manager.close_active_window()
-                elif e.key_code == ord('Q'):
-                    manager.recover_windows()
-                    sys.exit(0)
-            else:
-                if e.key_code == ord('A'):
-                    manager.switch_to_nth_stack(0)
-                elif e.key_code == ord('S'):
-                    manager.switch_to_nth_stack(1)
-                elif e.key_code == 13:    #enter
-                    manager.move_active_to_main_stack()
-                elif e.key_code == 0xBC:    #comma
-                    manager.change_max_main(+1)
-                elif e.key_code == 0xBE:    #period
-                    manager.change_max_main(-1)
+        if e.event_type == 'key down':
+            if mod_flag & self.noconvert:
+                if mod_flag & self.shift:
+                    if e.key_code == ord('A'):
+                        manager.send_active_window_to_nth_stack(0)
+                    elif e.key_code == ord('S'):
+                        manager.send_active_window_to_nth_stack(1)
+                    elif e.key_code == ord('C'):
+                        manager.close_active_window()
+                    elif e.key_code == ord('Q'):
+                        manager.recover_windows()
+                        sys.exit(0)
+                else:
+                    if e.key_code == ord('A'):
+                        manager.switch_to_nth_stack(0)
+                    elif e.key_code == ord('S'):
+                        manager.switch_to_nth_stack(1)
+                    elif e.key_code == 13:    #enter
+                        manager.move_active_to_main_stack()
+                    elif e.key_code == 0xBC:    #comma
+                        manager.change_max_main(+1)
+                    elif e.key_code == 0xBE:    #period
+                        manager.change_max_main(-1)
         print(e)
 
 if __name__ == '__main__':
     manager = window_manager.WindowManager(ignore_list=['TaskManagerWindow'])
-    manager.move_n_resize()
 
     entry = EntryPoint(manager)
     handle = keyhandler.KeyHandler()
     handle.handlers.append(entry.print_event)
-    handle.listen()
+
+    hookThread = threading.Thread(target=handle.listen)
+    hookThread.start()
+
+    while True:
+        manager.move_n_resize()
+        time.sleep(2)
+
+    hookThread.join()
