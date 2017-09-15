b6cfcfbcc90c48366b39d119525d30821c214178
Yoshioka Shun <tajas2006@yahoo.co.jp>
2017-09-15 12:08:44 +0900

    add comments

--- keyhandler.py
+++ keyhandler.py
@@ -1,9 +1,8 @@
-from collections import namedtuple
-import sys
+# based on https://gist.github.com/ethanhs/80f0a7cc5c7881f5921f
+#
 
-# KeyboardEvent = namedtuple('KeyboardEvent', ['event_type', 'key_code',
-                                             # 'scan_code', 'alt_pressed',
-                                             # 'time'])
+from collections import namedtuple
+
 KeyboardEvent = namedtuple('KeyboardEvent', ['event_type', 'key_code'])
 
 class KeyHandler:
@@ -18,16 +17,15 @@ class KeyHandler:
 
     def listen(self):
         """
-        Calls `handlers` for each keyboard event received. This is a blocking call.
+        Calls `handlers` for each keyboard event received.
+        This is a blocking call.
         """
         # Adapted from http://www.hackerthreads.org/Topic-42395
         from ctypes import windll, CFUNCTYPE, POINTER, c_int, c_void_p, byref
         import win32con, win32api, win32gui, atexit
 
         event_types = {win32con.WM_KEYDOWN: 'key down',
-                    win32con.WM_KEYUP: 'key up',
-                    0x104: 'key down', # WM_SYSKEYDOWN, used for Alt key.
-                    0x105: 'key up', # WM_SYSKEYUP, used for Alt key.
+                    win32con.WM_KEYUP: 'key up'
                     }
 
         def low_level_handler(nCode, wParam, lParam):
@@ -35,12 +33,7 @@ class KeyHandler:
             Processes a low level Windows keyboard event.
             """
             key_code = lParam[0] & 0xFFFFFFFF
-            # event = KeyboardEvent(event_types[wParam], lParam[0], lParam[1],
-                                # lParam[2] == 32, lParam[3])
             event = KeyboardEvent(event_types[wParam], key_code)
-
-            # if key_code == 13:     # enter
-            #     sys.exit(0)
 
             if key_code == 29:     # noconvert down
                 if event_types[wParam] == "key down":
@@ -68,18 +61,15 @@ class KeyHandler:
                 elif event_types[wParam] == "key up":
                     self.mod_flag -= self.shift
 
-            print ("{0:b}".format(self.mod_flag))
-            # Be a good neighbor and call the next hook.
-            # > meaning send the key press through
-            # return 1      # this will stop that
-            # return windll.user32.CallNextHookEx(hook_id, nCode, wParam, lParam)
-
             if self.mod_flag & self.noconvert:
                 for handler in self.handlers:
                     handler(event, self.mod_flag)
+                # capture the key press
                 return 1
             else:
-                return windll.user32.CallNextHookEx(hook_id, nCode, wParam, lParam)
+                # let the key press go
+                return windll.user32.CallNextHookEx(hook_id, nCode,
+                                                    wParam, lParam)
 
 
         # Our low level handler signature.
@@ -88,11 +78,14 @@ class KeyHandler:
         pointer = CMPFUNC(low_level_handler)
 
         # Hook both key up and key down events for common keys (non-system).
-        hook_id = windll.user32.SetWindowsHookExA(win32con.WH_KEYBOARD_LL, pointer,
-                                                win32api.GetModuleHandle(None), 0)
+        hook_id = windll.user32.SetWindowsHookExA(win32con.WH_KEYBOARD_LL,
+                                                pointer,
+                                                win32api.GetModuleHandle(None),
+                                                0
+                                                )
 
-        # Register to remove the hook when the interpreter exits. Unfortunately a
-        # try/finally block doesn't seem to work here.
+        # Register to remove the hook when the interpreter exits.
+        # Unfortunately a try/finally block doesn't seem to work here.
         atexit.register(windll.user32.UnhookWindowsHookEx, hook_id)
 
         while True:
