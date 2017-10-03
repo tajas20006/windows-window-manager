f249d127c78f13df5017cb5827393e9c62ee94fe
Yoshioka Shun <tajas2006@yahoo.co.jp>
2017-10-03 13:33:36 +0900

    rename config file

---
+++ config.py
@@ -0,0 +1,25 @@
+import constants as C
+
+ignore_list = [
+    {"class_name": "Windows.UI.Core.CoreWindow"},
+    {"class_name": "TaskManagerWindow"},
+    {"class_name": "Microsoft-Windows-SnipperToolbar"},
+    {"class_name": "Qt5QWindowIcon", "title": "GtransWeb"},
+    {"class_name": "screenClass"}
+    ]
+workspace_n = 9
+network_interface = "Dell Wireless 1820A 802.11ac"
+
+# config_list = [
+#         {"mod": C.NOCONVERT, "key": "q",
+#             "function": "focus_up", "param": [+1]}
+#         ]
+
+# remove_list = [
+#         {"mod": C.NOCONVERT, "key": "1"}
+#         ]
+
+# append_list = [
+#         {"mod": C.NOCONVERT | C.WIN, "key": "a", "function": "focus_up",
+#             "param": [+1]}
+#         ]
