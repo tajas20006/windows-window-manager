5ede91bb8efb32ca78464480639854b43cf8a729
Yoshioka Shun <tajas2006@yahoo.co.jp>
2017-10-03 13:34:29 +0900

    enable config file

--- main.py
+++ main.py
@@ -2,7 +2,10 @@ import keyhandler
 import windowhandler
 import windowmanager
 import constants as C
-
+try:
+    import config
+except ModuleNotFoundError:
+    print("no config file found. using default")
 
 import sys
 import threading
@@ -14,71 +17,94 @@ import win32gui
 
 
 class EntryPoint():
-    def __init__(self, manager, mod_key=C.NOCONVERT):
-        self.mod_key = mod_key
-        self.manager = manager
-        self.flag = True
-        self.config_list = []
-        self.set_default_config()
-
+    def __init__(self):
         try:
-            import config
+            self.mod_key = config.mod_key
+        except (AttributeError, NameError):
+            self.mod_key = C.NOCONVERT
+        try:
+            ignore_list = config.ignore_list
+        except (AttributeError, NameError):
+            ignore_list = []
+        try:
+            workspace_n = config.workspace_n
+        except (AttributeError, NameError):
+            workspace_n = 2
+        try:
+            network_interface = config.network_interface
+        except (AttributeError, NameError):
+            network_interface = ""
+
+        self.manager = windowmanager.WindowManager(
+                ignore_list=ignore_list,
+                workspace_n=workspace_n,
+                network_interface=network_interface
+                )
+
+        self.config_list = [
+                {"mod": self.mod_key | C.SHIFT, "key": "Q",
+                    "function": self.kill_program}
+                ]
+        try:
+            for a in config.config_list:
+                a["function"] = getattr(self.manager, a["function"], None)
+            self.config_list.extend(config.config_list)
+        except (AttributeError, NameError):
+            self.set_default_config()
             try:
-                self.config_list = config.config_list
-            except NameError:
-                try:
-                    self.remove_config(config.remove_list)
-                    self.append_config(config.append_list)
-                except NameError:
-                    pass
-        except ModuleNotFoundError:
-            print("config file not found. using default")
+                self.remove_config(config.remove_list)
+            except (AttributeError, NameError):
+                pass
+            try:
+                self.append_config(config.append_list)
+            except (AttributeError, NameError):
+                pass
 
     def set_default_config(self):
-        self.config_list = [
+        self.config_list.extend([
                 {"mod": self.mod_key | C.SHIFT, "key": "Q",
                     "function": self.kill_program},
                 {"mod": self.mod_key | C.SHIFT, "key": "C",
-                    "function": manager.close_window},
+                    "function": self.manager.close_window},
                 {"mod": C.NOCONVERT | C.SHIFT, "key": "J",
-                    "function": manager.swap_windows, "param": [+1]},
+                    "function": self.manager.swap_windows, "param": [+1]},
                 {"mod": C.NOCONVERT | C.SHIFT, "key": "K",
-                    "function": manager.swap_windows, "param": [-1]},
+                    "function": self.manager.swap_windows, "param": [-1]},
                 {"mod": C.NOCONVERT, "key": C.ENTER,
-                    "function": manager.swap_master},
+                    "function": self.manager.swap_master},
                 {"mod": C.NOCONVERT, "key": C.COMMA,
-                    "function": manager.inc_master_n, "param": [+1]},
+                    "function": self.manager.inc_master_n, "param": [+1]},
                 {"mod": C.NOCONVERT, "key": C.PERIOD,
-                    "function": manager.inc_master_n, "param": [-1]},
+                    "function": self.manager.inc_master_n, "param": [-1]},
                 {"mod": C.NOCONVERT, "key": C.TAB,
-                    "function": manager.focus_up, "param": [+1]},
+                    "function": self.manager.focus_up, "param": [+1]},
                 {"mod": C.NOCONVERT, "key": "J",
-                    "function": manager.focus_up, "param": [+1]},
+                    "function": self.manager.focus_up, "param": [+1]},
                 {"mod": C.NOCONVERT, "key": "K",
-                    "function": manager.focus_up, "param": [-1]},
+                    "function": self.manager.focus_up, "param": [-1]},
                 {"mod": C.NOCONVERT, "key": "I",
-                    "function": manager.show_window_info},
+                    "function": self.manager.show_window_info},
                 {"mod": C.NOCONVERT, "key": "H",
-                    "function": manager.expand_master, "param": [-20]},
+                    "function": self.manager.expand_master, "param": [-20]},
                 {"mod": C.NOCONVERT, "key": "L",
-                    "function": manager.expand_master, "param": [+20]},
+                    "function": self.manager.expand_master, "param": [+20]},
                 {"mod": C.NOCONVERT, "key": C.SPACE,
-                    "function": manager.next_layout},
+                    "function": self.manager.next_layout},
                 {"mod": C.NOCONVERT, "key": "D",
-                    "function": manager.toggle_caption},
+                    "function": self.manager.toggle_caption},
                 {"mod": C.NOCONVERT, "key": "A",
-                    "function": manager.arrange_windows},
+                    "function": self.manager.arrange_windows},
                 {"mod": C.NOCONVERT, "key": "Q",
-                    "function": manager.reset_windows}
-                ]
-        for i in range(manager.workspace_n):
+                    "function": self.manager.reset_windows}
+                ])
+        for i in range(self.manager.workspace_n):
             self.config_list.append({
                 "mod": C.NOCONVERT | C.SHIFT,
                 "key": str(i+1),
-                "function": manager.send_to_nth_ws, "param": [i]
+                "function": self.manager.send_to_nth_ws, "param": [i]
                 })
             self.config_list.append({"mod": C.NOCONVERT, "key": str(i+1),
-                "function": manager.switch_to_nth_ws, "param": [i]})
+                "function": self.manager.switch_to_nth_ws, "param": [i]})
 
     def remove_config(self, remove_list):
         r_list = []
@@ -91,6 +117,8 @@ class EntryPoint():
             self.config_list.remove(conf)
 
     def append_config(self, append_list):
+        for a in append_list:
+            a["function"] = getattr(self.manager, a["function"], None)
         self.config_list.extend(append_list)
 
     def print_event(self, e, mod_flag):
@@ -114,14 +142,14 @@ class EntryPoint():
                         return
 
     def kill_program(self):
-        manager.recover_windows()
+        self.manager.recover_windows()
         ctypes.windll.user32.PostThreadMessageW(
                 hook_thread.ident,
                 win32con.WM_QUIT,
                 0, 0
                 )
         ctypes.windll.user32.PostThreadMessageW(
-                manager.thr.ident,
+                self.manager.thr.ident,
                 win32con.WM_QUIT,
                 0, 0
                 )
@@ -130,74 +158,61 @@ class EntryPoint():
                 win32con.WM_QUIT,
                 0, 0,
                 )
-        self.flag = False
 
     def print_event_orig(self, e, mod_flag):
         if e.event_type == 'key down':
             if mod_flag & self.mod_key:
                 if mod_flag & C.SHIFT:
-                    for i in range(manager.workspace_n):
+                    for i in range(self.manager.workspace_n):
                         if e.key_code == ord(str(i+1)):
-                            manager.send_to_nth_ws(i)
+                            self.manager.send_to_nth_ws(i)
                     if e.key_code == ord('C'):
-                        manager.close_window()
+                        self.manager.close_window()
                     elif e.key_code == ord('Q'):
                         self.kill_program()
                     elif e.key_code == ord('J'):
-                        manager.swap_windows(+1)
+                        self.manager.swap_windows(+1)
                     elif e.key_code == ord('K'):
-                        manager.swap_windows(-1)
+                        self.manager.swap_windows(-1)
                 else:
-                    for i in range(manager.workspace_n):
+                    for i in range(self.manager.workspace_n):
                         if e.key_code == ord(str(i+1)):
-                            manager.switch_to_nth_ws(i)
+                            self.manager.switch_to_nth_ws(i)
                     if e.key_code == 0x0D:    # enter
-                        manager.swap_master()
+                        self.manager.swap_master()
                     elif e.key_code == 0xBC:    # comma
-                        manager.inc_master_n(+1)
+                        self.manager.inc_master_n(+1)
                     elif e.key_code == 0xBE:    # period
-                        manager.inc_master_n(-1)
+                        self.manager.inc_master_n(-1)
                     elif e.key_code == 0x09:    # tab
-                        manager.focus_up(+1)
+                        self.manager.focus_up(+1)
                     elif e.key_code == ord('J'):
-                        manager.focus_up(+1)
+                        self.manager.focus_up(+1)
                     elif e.key_code == ord('K'):
-                        manager.focus_up(-1)
+                        self.manager.focus_up(-1)
                     elif e.key_code == ord('I'):
-                        manager.show_window_info()
+                        self.manager.show_window_info()
                     elif e.key_code == ord('H'):
-                        manager.expand_master(-20)
+                        self.manager.expand_master(-20)
                     elif e.key_code == ord('L'):
-                        manager.expand_master(+20)
+                        self.manager.expand_master(+20)
                     elif e.key_code == 0x20:    # space
-                        manager.next_layout()
+                        self.manager.next_layout()
                     elif e.key_code == ord('D'):
-                        manager.toggle_caption()
+                        self.manager.toggle_caption()
                     elif e.key_code == ord('A'):
-                        manager.arrange_windows()
+                        self.manager.arrange_windows()
                     elif e.key_code == ord('Q'):
-                        manager.reset_windows()
+                        self.manager.reset_windows()
 
     def window_changed(self):
-        if manager.watch_dog():
-            manager.arrange_windows()
+        if self.manager.watch_dog():
+            self.manager.arrange_windows()
 
 if __name__ == '__main__':
-    manager = windowmanager.WindowManager(
-            ignore_list=[
-                {"class_name": "Windows.UI.Core.CoreWindow"},
-                {"class_name": "TaskManagerWindow"},
-                {"class_name": "Microsoft-Windows-SnipperToolbar"},
-                {"class_name": "Qt5QWindowIcon", "title": "GtransWeb"},
-                {"class_name": "screenClass"}
-                ],
-            workspace_n=9,
-            network_interface="Dell Wireless 1820A 802.11ac"
-            )
 
-    mod_key = C.NOCONVERT
-    entry = EntryPoint(manager, mod_key)
-    key_handler = keyhandler.KeyHandler(mod_key)
+    entry = EntryPoint()
+    key_handler = keyhandler.KeyHandler(entry.mod_key)
     key_handler.handlers.append(entry.print_event)
     window_handler = windowhandler.WindowHandler(entry.window_changed)
     window_thread = threading.Thread(target=window_handler.main)
@@ -205,19 +220,16 @@ if __name__ == '__main__':
 
     hook_thread = threading.Thread(target=key_handler.listen)
     hook_thread.start()
-    counter = 0
-        # if manager.watch_dog():
-        #     manager.arrange_windows()
     try:
 
         while True:
-            manager.show_system_info()
-            manager._update_taskbar_text()
+            entry.manager.show_system_info()
+            entry.manager._update_taskbar_text()
             time.sleep(0.5)
     except:
         hook_thread.join()
         window_thread.join()
-        manager.thr.join()
+        entry.manager.thr.join()
         win32gui.MessageBox(
                 None,
                 "window manager exit",
